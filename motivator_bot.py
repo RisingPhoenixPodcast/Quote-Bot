c61bd95a0c59c442534eba61b059196e07665349
RisingPhoenixPodcast <RisingPhoenixPodcast2020@gmail.com>
2025-04-23 21:56:34 -0400

    Update motivator_bot.py

--- motivator_bot.py
+++ motivator_bot.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import tasks, commands
 import datetime
+from datetime import timezone, timedelta
 import random
 import os
 import json
@@ -23,14 +24,27 @@ intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 
+# Define the Eastern Time function
+def get_eastern_time():
+    # Check if we're in Daylight Saving Time
+    now = datetime.datetime.now()
+    is_dst = now.astimezone().dst() != timedelta(0)
+    
+    if is_dst:
+        # Eastern Daylight Time (UTC-4)
+        return datetime.time(hour=12, minute=0)  # 8 AM EDT = 12 PM UTC
+    else:
+        # Eastern Standard Time (UTC-5)
+        return datetime.time(hour=13, minute=0)  # 8 AM EST = 13 PM UTC
+
 # When the bot is ready
 @bot.event
 async def on_ready():
     print(f"🤖 Logged in as {bot.user}")
     send_quote.start()
 
-# Daily quote post at 8 AM (UTC time)
-@tasks.loop(time=datetime.time(hour=8, minute=0))
+# Daily quote post at 8 AM (Eastern time)
+@tasks.loop(time=get_eastern_time())
 async def send_quote():
     channel = bot.get_channel(CHANNEL_ID)
     if channel:
